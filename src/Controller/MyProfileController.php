6c4c7e0ea4721646c01441e810de39257be3c844
paul <paul.schuhmacher2019@campus-eni.fr>
2020-02-13 14:40:34 +0100

    update myprofile - choose between location in base - routes

--- src/Controller/MyProfileController.php
+++ src/Controller/MyProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
 use App\Form\MyProfileType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,7 +18,9 @@ class MyProfileController extends AbstractController
     public function myProfile(Request $request, EntityManagerInterface $entityManager)
     {
         $user = $this->getUser();
+
         $memberForm = $this->createForm(MyProfileType::class, $user);
+
 
         $memberForm->handleRequest($request);
 
@@ -28,11 +31,13 @@ class MyProfileController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('display_events');
         }
 
-        return $this->render('my_profile/myProfile.html.twig', [
+        return $this->render('my_profile/myProfile.html.twig',
+            [
             'myProfileFormView' => $memberForm->createView(),
+
         ]);
     }
 
--- src/Form/MyProfileType.php
+++ src/Form/MyProfileType.php
@@ -3,11 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Member;
+use App\Entity\Site;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,15 +18,13 @@ class MyProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo', TextType::class, ['label'=>'Pseudo : ', 'required'=>true])
-            ->add('name', TextType::class, ['label'=>'Prénom : ', 'required'=>true])
-            ->add('surname', TextType::class, ['label'=>'Nom : ', 'required'=>true])
-            ->add('phone', TextType::class, ['label'=>'Téléphone : ', 'required'=>true])
-            ->add('mail', EmailType::class, ['label'=>'Email : ', 'required'=>true])
-//            ->add('password', RepeatedType::class,
-//                ['first_options'=> ['label'=>'Mot de passe : '], 'second_options'=>['label'=>'Confirmation :'],])
-            ->add('site', TextType::class, ['label'=>'Ville de rattachement : ', 'required'=>true])
-            ->add('save', SubmitType::class, ['label'=>'Ajouter',])
+            ->add('pseudo', TextType::class, ['label'=>'Pseudo ', 'required'=>true])
+            ->add('name', TextType::class, ['label'=>'Prénom ', 'required'=>true])
+            ->add('surname', TextType::class, ['label'=>'Nom ', 'required'=>true])
+            ->add('phone', TextType::class, ['label'=>'Téléphone', 'required' => false])
+            ->add('mail', EmailType::class, ['label'=>'Email ', 'required'=>true])
+            ->add('site', EntityType::class, ['class' => Site::class, 'choice_label' =>'name'])
+            ->add('save', SubmitType::class, ['label'=>'Enregistrer',])
         ;
     }
 
@@ -35,5 +33,6 @@ class MyProfileType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Member::class,
         ]);
+
     }
 }
