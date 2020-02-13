02b8f0023e9f0df2d2615302f6efb8978b64eb03
Ararhak <hakim.arari2019@campus-eni.fr>
2020-02-13 15:18:10 +0100

    modifying rule of starting date for event creation form

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -88,15 +88,8 @@ class Event
     public function validateStartingDate(ExecutionContextInterface $context, $payload)
     {
         $dateadd =$this->getStartingDateTime();
-        if(!is_numeric($this->getDuration())){
-            return;
-        }
-        $dur='PT'.$this->getDuration().'H';
-        $duration = new \DateInterval($dur);
-        $dateadd = new \DateTime();
-        $dateadd->add($duration);
         if($dateadd<new \DateTime('now')){
-            $context->buildViolation('La date de début et la durée doivent être supérieures à maintenant')
+            $context->buildViolation('La date de début ne peut pas être dans le passé')
                 ->atPath('startingDateTime')
                 ->addViolation();
         }
@@ -113,7 +106,7 @@ class Event
         $duration = new\DateInterval('PT'.$this->getDuration().'H');
         $dateadd = new \DateTime();
         $dateadd = $this->getStartingDateTime()->add($duration);
-        if ($this->getInscriptionDeadLine() > $dateadd) {
+        if ($this->getInscriptionDeadLine() > $this->getStartingDateTime()) {
             $context->buildViolation(
                 'La date limite d\inscriptionde doit être inférieure à la date de début et la durée de l\'évenement'
             )
