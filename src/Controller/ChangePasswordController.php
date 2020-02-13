9c8e4140f7042809cbb78733e4ec71aeb49f9822
Romain MERCIER <romain.mercier2019@campus-eni.fr>
2020-02-13 11:19:12 +0100

    Change password with many bugs. Paul is fixing the problem

--- src/Controller/ChangePasswordController.php
+++ src/Controller/ChangePasswordController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Member;
 use App\Form\ChangePasswordType;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,10 +18,10 @@ class ChangePasswordController extends AbstractController
      */
     public function changePassword(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
     {
-
+    $oldPassword = $this->getUser()->getPassword();
         //require the user to log during the session, not based on a 'remember_me' cookie
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
+        $currentUserBDD= $entityManager->getRepository(Member::class)->find($this->getUser()->getId());
         $user = $this->getUser();
 
 
@@ -33,7 +34,7 @@ class ChangePasswordController extends AbstractController
 
 
 
-        if($changePasswordForm->isSubmitted() && $changePasswordForm->isValid() && $changePasswordForm != null){
+        if($changePasswordForm->isSubmitted() && $changePasswordForm->isValid()){
             //TODO : verifier toutes les contraintes de taille de caractères spéciaux, pas le meme que l'ancien
 
 
@@ -42,11 +43,10 @@ class ChangePasswordController extends AbstractController
                 $user,
                 $changePasswordForm->get('currentPassword')->getData()
             );
-
-            $currentUserBDD= $entityManager->getRepository(Member::class)->find($this->getUser()->getId());
+            
             
 
-            if($passwordEncoder->isPasswordValid($currentUserBDD, $hashPasswordCapture)) {
+            if($passwordEncoder->isPasswordValid($currentUserBDD, $changePasswordForm->get('currentPassword')->getData())) {
 
 
                 //encode the password give in the form
@@ -69,7 +69,7 @@ class ChangePasswordController extends AbstractController
 
             } else {
                 //TODO
-                dump($currentUserBDD, $hashPasswordCapture);
+                dump($currentUserBDD, $changePasswordForm->get('currentPassword')->getData());
                 die();
             }
 
