dd54385356586a7cef4ef9bc6135afd52a5bc3d5
Ararhak <hakim.arari2019@campus-eni.fr>
2020-02-14 09:20:58 +0100

    ajax request to enable filtering by sites

--- src/Controller/DisplayListEventsController.php
+++ src/Controller/DisplayListEventsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\Event;
+use App\Entity\Site;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +18,19 @@ class DisplayListEventsController extends AbstractController
     public function displayEvents(EntityManagerInterface $entityManager){
         $site = $this->getUser()->getSite();
         $eventRepo = $entityManager->getRepository(Event::class);
+        $sites = $entityManager->getRepository(Site::class)->findAll();
         $events = $eventRepo->findEventBySite($site);
-        return $this->render("displayevents/displayevents.html.twig",compact('events'));
+        return $this->render("displayevents/displayevents.html.twig",compact('events','sites'));
+    }
+
+    /**
+     * @Route("/displayevents/{site}", name="display_events_by_site")
+     */
+    public function displayEventsbySite(EntityManagerInterface $entityManager, $site){
+
+        $eventRepo = $entityManager->getRepository(Event::class);
+        $events = $eventRepo->findEventBySite($site);
+        return $this->render("includes/eventslist.html.twig",compact('events'));
     }
 
 }
