c846fa37745327346383e32357f33907057abf16
paul <paul.schuhmacher2019@campus-eni.fr>
2020-02-20 11:55:17 +0100

    wip : import csv ok

--- src/Controller/ImportCsvController.php
+++ src/Controller/ImportCsvController.php
@@ -13,28 +13,67 @@ class ImportCsvController extends AbstractController
 
 
     /**
-     * @Route("/admin/import-users-csv", name="import_user_csv");
+     * @Route("/admin/import-users-csv/{file_to_import}", name="import_user_csv");
      * */
-    public function importUserCSV(EntityManagerInterface $entityManager)
+    public function importMembersFromCSV(EntityManagerInterface $entityManager, $file_to_import = 'listeMembres.csv')
     {
 
+        $pathToFile = $this->getParameter('data_csv_directory').'/'.$file_to_import;
 
-//        if( ($fileCSV = fopen( '../assests/data/listeMembres.csv','r')) !== false){
-        if(file_exists ('assests/data/listeMembres.csv')){
-
-            dump('file open');
-        }
-        else{
-            dump('error');
+        if (!$this->fileToImportExist($pathToFile)) {
+            $this->addFlash('error', 'Impossible de trouver le ficher à importer!');
+            return $this->redirectToRoute('display_events');
         }
 
+        $data = $this->loadCSVtoArray($pathToFile);
 
+        if (!$data) {
+            $this->addFlash('error', 'Erreur dans l importation du fichier, verifier le format!');
+            return $this->redirectToRoute('display_events');
+        }
 
-
-
+        dump('loaded ok');
         die();
 
-        return $this->redirectToRoute('app_login');
+        //Send data to AdminController route 'app_register_from_csv'
+
     }
 
+
+    public function fileToImportExist($pathToFile)
+    {
+        return file_exists($pathToFile);
+    }
+
+
+    public function loadCSVtoArray($pathToFile)
+    {
+        if (file_exists($pathToFile)) {
+
+            $dataALL = array();
+
+            $nbFieldsExpectedAtEachRow = 4;
+
+            if (($handlerCSV = fopen($pathToFile, 'r')) !== false) {
+
+                while ( ($data = fgetcsv($handlerCSV, 500, ",") ) !== false) {
+
+                    $num = count($data);
+
+                    $dataALL[] = $data;
+
+                    if (count($data) !== $nbFieldsExpectedAtEachRow) {
+                        return false;
+                    }
+
+                }
+                fclose($handlerCSV);
+            }
+
+            return $dataALL;
+        }
+        else{
+            return false;
+        }
+    }
 }
